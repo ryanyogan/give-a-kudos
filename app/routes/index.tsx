6ffb77db1f6993f9c52f0f10df84a49ca532b8d6
Ryan Yogan <ryanyogan@gmail.com>
2022-06-01 20:30:49 -0500

    running

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div className="h-screen antialiased bg-gray-50 flex justify-center items-center">
+      <h1 className="text-slate-800 text-xl font-semibold text-center">
+        Hello World
+      </h2>
+    </div>
+  );
+}
